06319132ec965bf331dc6307c5cc1d9511f7abec
atakeuchii <takeuchi.akihide1110@gmail.com>
2026-07-22 21:25:48 +0900

    feat: 検索機能を実装し、AND/OR条件での文書ID取得を追加

--- test/my_text_search/store_test.clj
+++ test/my_text_search/store_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer [deftest is]]
             [my-storage.core :as lsm]
             [my-text-search.index :as idx]
+            [my-text-search.query :as q]
             [my-text-search.store :as store]))
 
 (defn- temp-dir []
@@ -31,4 +32,38 @@
       (is (= (sorted-set 0)   (store/get-posting db "本酒")))
       (is (= "日本酒の醸造"    (store/get-doc db 0)))
       (is (= 3 (store/get-next-id db)))
+      (lsm/close db))))
+
+(deftest q-search-over-store
+  (let [dir (temp-dir)]
+    (let [ix (reduce idx/add-document (idx/empty-index)
+                     ["日本酒の醸造" "純米大吟醸の精米" "日本語の文法"])
+          db (lsm/open dir)]
+      (store/persist-index! db ix)
+      (lsm/close db))
+    (let [db (lsm/open dir)
+          pf #(store/get-posting db %)]
+      (is (= [0] (vec (q/search pf "日本酒"))))
+      (is (= [0 2] (vec (q/search pf "日本酒" :op :or))))
+      (is (= [2] (vec (q/search pf "日本語"))))
+      (is (= [] (vec (q/search pf "焼酎"))))
+      (lsm/close db))))
+
+(deftest search-docs-attaches-text
+  (let [dir (temp-dir)]
+    (let [ix (reduce idx/add-document (idx/empty-index)
+                     ["日本酒の醸造" "日本語の文法"])
+          db (lsm/open dir)]
+      (store/persist-index! db ix)
+      (lsm/close db))
+    (let [db (lsm/open dir)
+          pf #(store/get-posting db %)
+          df #(store/get-doc db %)]
+      (is (= [{:doc-id 0 :text "日本酒の醸造"}]
+             (q/search-docs pf df "日本酒")))
+      (is (= [{:doc-id 0 :text "日本酒の醸造"}
+              {:doc-id 1 :text "日本語の文法"}]
+             (q/search-docs pf df "日本" :op :or)))
+      (is (= [{:doc-id 0 :text "日本酒の醸造"}]
+             (q/search-docs pf df "日本" :op :or :limit 1)))
       (lsm/close db))))
