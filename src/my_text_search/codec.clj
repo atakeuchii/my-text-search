045d3f675f3154df599cfbabd9b75433d38fb722
atakeuchii <takeuchi.akihide1110@gmail.com>
2026-07-29 23:22:23 +0900

    feat: 文書内の語順・隣接を考慮したフレーズ検索機能を追加

--- src/my_text_search/codec.clj
+++ src/my_text_search/codec.clj
@@ -29,32 +29,35 @@
     (.toByteArray out)))
 
 (defn encode-posting
-  "ソート済み {文書ID -> TF} をバイト列へ符号化する。入力は doc-id 昇順であること。
-   [flags(1B)][doc-count varint][delta-doc-id varint...][tf varint...]
-   flags bit0=1: TF セクションあり。"
-  ^bytes [doc->tf]
+  "ソート済み {文書ID -> [位置(昇順)]} をバイト列へ。入力は doc-id 昇順であること。
+   [flags(1B)=2][doc-count][delta-doc-id...][各doc: [pos-count][delta-pos...]]
+   flags bit1=1: 位置セクションあり。tf は位置数から導出する。"
+  ^bytes [doc->pos]
   (let [out (ByteArrayOutputStream.)
-        entries (seq doc->tf)
+        entries (seq doc->pos)
         ids (map key entries)
-        tfs (map val entries)]
-    (.write out (int 1))
+        poss (map val entries)]
+    (.write out (int 2))
     (write-uvarint! out (count entries))
     (loop [prev 0
            es ids]
       (when (seq es)
         (write-uvarint! out (- (long (first es)) prev))
         (recur (long (first es)) (rest es))))
-    (doseq [tf tfs]
-      (write-uvarint! out (long tf)))
+    (doseq [ps poss]
+      (write-uvarint! out (count ps))
+      (loop [prev 0
+             es (seq ps)]
+        (when (seq es)
+          (write-uvarint! out (- (long (first es)) prev))
+          (recur (long (first es)) (rest es)))))
     (.toByteArray out)))
 
 (defn decode-posting
-  "バイト列を {文書ID -> TF}(sorted-map) に復元する。
-   flags の bit0 で TF セクションの有無を判定。旧形式(TFなし)は tf=1 とみなす。"
+  "バイト列を {文書ID -> [位置]}(sorted-map) に復元する。"
   [^bytes bs]
   (let [buf (ByteBuffer/wrap bs)
-        flags (bit-and (long (.get buf)) 0xff)
-        has-tf? (pos? (bit-and flags 1))
+        _flags (bit-and (long (.get buf)) 0xff)
         n (read-uvarint buf)
         ids (loop [i 0
                    prev 0
@@ -63,11 +66,17 @@
                 (let [id (+ prev (read-uvarint buf))]
                   (recur (inc i) id (conj! acc id)))
                 (persistent! acc)))
-        tfs (if has-tf?
-              (loop [i 0
-                     acc (transient [])]
-                (if (< i n)
-                  (recur (inc i) (conj! acc (read-uvarint buf)))
-                  (persistent! acc)))
-              (repeat n 1))]
-     (into (sorted-map) (map vector ids tfs))))
+        poss (loop [i 0
+                    acc (transient [])]
+               (if (< i n)
+                 (let [pc (read-uvarint buf)
+                       ps (loop [j 0
+                                 prev 0
+                                 pacc (transient [])]
+                            (if (< j pc)
+                              (let [p (+ prev (read-uvarint buf))]
+                                (recur (inc j) p (conj! pacc p)))
+                              (persistent! pacc)))]
+                   (recur (inc i) (conj! acc ps)))
+                 (persistent! acc)))]
+    (into (sorted-map) (map vector ids poss))))
